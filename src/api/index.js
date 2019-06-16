deaf678be148d2fb716e3bcc35e0fd8a448f581c
dundja92 <dundja3@gmail.com>
2019-06-17 01:26:34 +0200

    CORS fix

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://api.themoviedb.org/3"
+    baseURL: "https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3"
 });
